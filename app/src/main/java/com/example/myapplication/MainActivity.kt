7a5910ff9be75920319f4c06c2ef42ea75a51d36
MadaraShogan <deepanshusingh2812@gmail.com>
2025-05-02 23:38:38 +0530

    Nothing

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,13 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApplication(){
-    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) { }
+    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+        Card (modifier =Modifier.width(200.dp).height(390.dp),
+            shape= RoundedCornerShape(corner = CornerSize(15.dp)),
+//            colors = CardDefaults.cardColors(containerColor = Color.Green),
+//            elevation = 4.dp
+                ){
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
